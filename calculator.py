ffa576a6a1ab437bee0db47263dc14f95eb8fe2c
Iqrakhalid17 <125212619+Iqrakhalid17@users.noreply.github.com>
2024-03-22 05:08:34 -0700

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,33 +1,26 @@
-def sum(op1, op2):
-  print("the addition of numbers is : " , op1+op2 )
-def sub(op1, op2):
-  print("the subtraction of numbers is : " , op1-op2)
-def mul(op1, op2):
-  print("the multiplication of numbers is : ", op1*op2)
-def div(op1, op2):
-  print("the division of numbers is : ")
-  if op2 == 0:
-   print("division is not possible")
-  else:
-   print("the division of numbers is : " , op1/op2)
-def rem(op1, op2):
-  print("the remainder of numbers is : " , op1%op2)
 
-def calculator(op1,op2,operation):
-   if operation == '+':
-    sum(op1,op2)
-   elif operation == '-':
-    sub(op1,op2)
-   elif operation == '*':
-    mul(op1,op2)
-   elif operation == '/':
-    div(op1,op2)
-   elif operation == '% ':
-    rem(op1,op2)
-   else:
-    print("invalid operation")
-  
-op1 = int(input("enter the first number : "))
-op2 = int(input("enter the second number : "))
-operation = input("enter the operation() : ",)
-calculator(op1,op2,operation)
+# coding: utf-8
+
+# In[ ]:
+
+# for addition
+def addition(x,y):
+    return x+y
+
+# for subtraction
+def subtraction(x,y):
+    return x-y
+
+# for multiplacation
+def multiplacation(x,y):
+    return x*y
+
+# for division
+def division(x,y):
+    return x/y
+
+# for reminder
+def reminder(x,y):
+    return x%y
+    
+
